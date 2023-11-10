18cda8ad17dd7f5c20cce769753244adc897a569
Mickael Roger <mickael@mickael-roger.com>
2023-11-10 21:14:21 +0100

    Update init

--- config-files/neovim/init.lua
+++ config-files/neovim/init.lua
@@ -277,6 +277,9 @@ vim.o.termguicolors = true
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+-- Remap key for leaving term
+vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]])
+
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
