cb58db3656d04f6864b45af1d1871e7a536994bf
Mickael Roger <mickael@mickael-roger.com>
2024-09-28 21:32:53 +0200

    Add laptop

---
+++ hosts/xps-laptop/configuration.nix
@@ -0,0 +1,25 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, nur, ... }:
+
+{
+  imports =
+    [ 
+      ../../common/base.nix
+      ../../common/pkgs/cybersec.nix
+      ../../common/pkgs/kube.nix
+      ../../common/pkgs/web.nix
+      ../../common/pkgs/tools.nix
+      ../../common/pkgs/virtu.nix
+      ../../common/pkgs/media.nix
+      ../../common/pkgs/cloud.nix
+      ../../common/pkgs/dev.nix
+      ../../common/pkgs/maker.nix
+    ];
+
+  networking.hostName = "xps-mick"; 
+
+
+}
