4d81635632263a1a1d2a8efc189aafde63390267
MickaelRoger <mickael@mickael-roger.com>
2026-01-02 15:29:30 +0100

    Remove lid

--- hosts/xps-laptop/configuration.nix
+++ hosts/xps-laptop/configuration.nix
@@ -22,5 +22,35 @@
 
   networking.hostName = "xps-mick"; 
 
+  # Power management for laptop - don't sleep when on AC power
+  powerManagement = {
+    enable = true;
+    # Don't suspend when lid is closed if external monitor is connected
+    logind = {
+      handleLidSwitch = "ignore";
+      handleLidSwitchExternalPower = "ignore";
+      handleLidSwitchDocked = "ignore";
+    };
+  };
+
+  # Additional power settings to prevent sleep on AC power
+  systemd.services."prevent-sleep-on-ac" = {
+    description = "Prevent sleep when on AC power";
+    wantedBy = [ "multi-user.target" ];
+    script = ''
+      #!${pkgs.bash}/bin/bash
+      while true; do
+        if [ "$(cat /sys/class/power_supply/AC/online 2>/dev/null || echo 0)" = "1" ]; then
+          # On AC power, prevent sleep
+          ${pkgs.systemd}/bin/systemd-inhibit --what=idle:sleep --why="On AC power" --mode=block sleep infinity
+        fi
+        sleep 60
+      done
+    '';
+    serviceConfig = {
+      Restart = "always";
+      RestartSec = "5";
+    };
+  };
 
 }
